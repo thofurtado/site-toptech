22533cfad55b312e99b8f8c99f44979c8db53b8a
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2020-07-14 18:40:03 -0300

    links completos
    finalizado os apontamentos internos para os downloads

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function Index(){
                 <nav class="tabs is-boxed is-fullwidth">
                     <div class="container">
                         <ul>
-                            <li><a class="has-text-danger" href="https://docs.google.com/uc?export=download&id=1Rvzaz_3cFzOKCmtgjapv3Zo2CVG95TK2">Anydesk</a></li>
-                            <li><a class="has-text-link" href="https://docs.google.com/uc?export=download&id=15MFO8E-tljFKCuSzyGTCq1QgNfz55dD_">Team Viewer</a></li>
+                            <li><a class="has-text-danger" href="https://apptoptech.vercel.app/Suporte_Anydesk.exe">Anydesk</a></li>
+                            <li><a class="has-text-link" href="https://apptoptech.vercel.app/Suporte_TeamViewer.exe">Team Viewer</a></li>
                             <li><a class="has-text-warning" href="https://apptoptech.vercel.app/Winrar.exe">Winrar</a></li>
                         </ul>
                     </div>
