9213eff032d24722d1f9551818a7e0891287c31e
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2020-07-14 14:32:16 -0300

    Initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.css';
+
+function App({Component, pageProps}){
+    return (
+        <>
+        <Component {...pageProps} />
+        </>
+    )
+}
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+
+function Index(){
+    return(
+        <section class="hero is-black is-fullheight">
+  
+  <div class="hero-head">
+    <header class="navbar">
+      <div class="container">
+        
+      </div>
+    </header>
+  </div>
+
+  
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title has-text-danger">
+        TOPTECH
+      </h1>
+      <h2 class="subtitle has-text-warning">
+        Suporte
+      </h2>
+    </div>
+  </div>
+  <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li><a href="https://download.anydesk.com/AnyDesk.exe?_ga=2.204996502.74908729.1594742025-1278563154.1594742025">Anydesk</a></li>
+          <li><a href="http://www.sistemaathos.com.br/files/suporteremoto.exe">Team Viewer</a></li>
+          <li><a href="https://www.win-rar.com/fileadmin/winrar-versions/winrar/wrar591.exe">Winrar</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
+    )
+}
+export default Index;
