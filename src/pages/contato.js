96db30e85cc5d0fc777eacd35429cca3bffe1cf3
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2020-07-15 00:57:46 -0300

    navegação e menus

---
+++ src/pages/contato.js
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+
+function Index(){
+    return(
+        <section class="hero is-black is-fullheight">
+        <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-end">
+            <Link href="/noticias">
+              <a class="navbar-item">Notícias</a>
+            </Link>
+            <Link href="/antivirus">
+              <a class="navbar-item">Antivírus</a>
+            </Link>
+            <Link href="/contato" >
+            <a class="navbar-item" id="isDisabled">Contato</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>    
+            
+              <div class="hero-body" id="header-toptech" >
+                <div class="container  has-text-centered">
+                    <h1 class="title text-flicker-in-glow">
+                        TOPTECH
+                    </h1>
+                    <h2 class="subtitle has-text-info roll-in-top">
+                        Contato
+                    </h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Index;
--- src/pages/noticias.js
+++ src/pages/noticias.js
@@ -7,12 +7,12 @@ function Index(){
     <nav class="navbar">
       <div class="container">
         <div class="navbar-brand">
-          
           <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-            
+            <span></span>
+            <span></span>
+            <span></span>
           </span>
         </div>
-        
         <div id="navbarMenuHeroA" class="navbar-menu">
           <div class="navbar-end">
             <Link href="/noticias">
@@ -30,26 +30,15 @@ function Index(){
     </nav>
   </div>    
             
-              <div class="hero-body" id="header-toptech">
-                <div class="container has-text-centered">
+              <div class="hero-body" id="header-toptech" >
+                <div class="container  has-text-centered">
                     <h1 class="title text-flicker-in-glow">
                         TOPTECH
                     </h1>
-                    <h2 class="subtitle has-text-success roll-in-top">
-                        Downloads
+                    <h2 class="subtitle has-text-danger roll-in-top">
+                        Notícias
                     </h2>
                 </div>
-            </div>
-            <div class="hero-foot">
-                <nav class="tabs is-boxed is-fullwidth">
-                    <div class="container">
-                        <ul>
-                            <li><a class="roll-in-left has-text-danger" href="https://apptoptech.vercel.app/Suporte_Anydesk.exe">Anydesk</a></li>
-                            <li><a class="roll-in-bottom has-text-link" href="https://apptoptech.vercel.app/Suporte_TeamViewer.exe">Team Viewer</a></li>
-                            <li><a class="roll-in-right has-text-warning" href="https://apptoptech.vercel.app/Winrar.exe">Winrar</a></li>
-                        </ul>
-                    </div>
-                </nav>
             </div>
         </section>
     )
